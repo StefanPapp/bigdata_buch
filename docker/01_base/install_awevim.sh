f13dd559e19014a6ea3a9d1c44f0dd243b9365dd
spapp <stefan.papp@icloud.com>
2018-07-07 23:13:39 +0400

    added vim config

---
+++ docker/01_base/install_awevim.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
--- docker/01_base/install_devenv.sh
+++ docker/01_base/install_devenv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y python python-pip openjdk-8-jdk
+apt-get install -y python3 python3-pip openjdk-8-jdk
 
 cd /usr/lib/jvm
 ln -s java-8-openjdk-amd64 jdk
